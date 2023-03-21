524989be98c888dbbb6c7f46b6cd0291e3c0e09f
jvictormorais <jose.alvarezmorais@gmail.com>
2023-03-21 16:23:26 -0300

    Add scroll effect

--- app.js
+++ app.js
@@ -17,3 +17,39 @@ function closeMenu(e) {
 
 menuBtn.addEventListener("click", showMenu);
 closeBtn.addEventListener("click", closeMenu);
+
+// scroll
+
+document.lastScrollPosition = 0;
+document.lastCentered = 0;
+document.onWayTo = null;
+
+document.addEventListener("scroll", () => {
+  const direction =
+    window.pageYOffset - document.lastScrollPosition > 0 ? "down" : "up";
+  const sections = [...document.querySelectorAll("section")];
+
+  if (document.onWayTo === null) {
+    const destIndex =
+      direction === "up"
+        ? document.lastCentered - 1
+        : document.lastCentered + 1;
+    if (destIndex >= 0 && sections.length) {
+      document.onWayTo = destIndex;
+      window.scroll(0, sections[destIndex].offsetTop);
+    }
+  }
+
+  sections.forEach((section, number) => {
+    if (window.pageYOffset === section.offsetTop) {
+      document.lastCentered = number;
+      section.className = "active";
+      if (document.onWayTo === number) {
+        document.onWayTo = null;
+      }
+    } else {
+      section.className = "";
+    }
+  });
+  document.lastScrollPosition = window.pageYOffset;
+});
